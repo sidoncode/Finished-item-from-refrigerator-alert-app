2cf62dcfb069453ba1bf8a6cfc86fac03aa28985
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2020-09-22 21:22:54 +0530

    charts activity

--- app/src/main/java/com/siddevlops/finisheditemfromrefrigeratoralertapp/DashboardActivity.java
+++ app/src/main/java/com/siddevlops/finisheditemfromrefrigeratoralertapp/DashboardActivity.java
@@ -11,7 +11,7 @@ import androidx.cardview.widget.CardView;
 
 public class DashboardActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private CardView mCardview;
+    private CardView Cardview1,Cardview2,Cardview3,Cardview4,Cardview5,Cardview6;
 
 
 
@@ -20,16 +20,30 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        mCardview = findViewById(R.id.cardview1);
-        mCardview.setOnClickListener(new View.OnClickListener() {
+        Cardview1 = findViewById(R.id.cardview1);
+        Cardview2 = findViewById(R.id.cardview2);
+        Cardview3 = findViewById(R.id.cardview3);
+        Cardview4 = findViewById(R.id.cardview4);
+        Cardview5 = findViewById(R.id.cardview5);
+        Cardview6 = findViewById(R.id.cardview6);
+
+        // set on click listerners
+        Cardview1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent acone = new Intent(DashboardActivity.this,ListingActivity.class);
-                startActivity(acone);
+                Intent ListingActivity = new Intent(DashboardActivity.this,ListingActivity.class);
+                startActivity(ListingActivity);
                 finish();
             }
         });
 
+
+        Cardview2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
 
 
